33e55f6687272479fe0ba36dfcf8c709e390f98d
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2021-03-08 11:09:03 -0300

    Create ListLength's call method

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,18 +1,3 @@
 defmodule ListLength do
-  @moduledoc """
-  Documentation for `ListLength`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListLength.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def call(list), do: length(list)
 end
